eeaa8ecd146a5c3fc3f9bd3e44e79402765c7c54
leona <leona@DESKTOP-QPSVVE9>
2025-04-26 16:20:23 -0500

    commit

--- Inventario/src/GuiPrincipal/GuiInventario.java
+++ Inventario/src/GuiPrincipal/GuiInventario.java
@@ -44,17 +44,8 @@ public class GuiInventario extends JFrame {
 		JMenu mnNewMenu = new JMenu("Menú Inventario");
 		menuBar.add(mnNewMenu);
 		
-		JMenuItem Menu_Agregar_Producto = new JMenuItem("Agregar producto");
-		mnNewMenu.add(Menu_Agregar_Producto);
-		
-		JMenuItem Menu_Modificar_Producto = new JMenuItem("Modificar producto");
-		mnNewMenu.add(Menu_Modificar_Producto);
-		
-		JMenuItem Menu_Eliminar_Producto = new JMenuItem("Eliminar producto");
-		mnNewMenu.add(Menu_Eliminar_Producto);
-		
-		JMenuItem Menu_Ver_Inventario_Completo = new JMenuItem("Ver inventario completo");
-		mnNewMenu.add(Menu_Ver_Inventario_Completo);
+		JMenuItem Menu_Producto = new JMenuItem("Producto");
+		mnNewMenu.add(Menu_Producto);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
