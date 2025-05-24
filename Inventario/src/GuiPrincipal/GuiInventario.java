7bd674dbcca8712c2ef81350d09cc2581bdff204
piero <piero@LAPTOP-C14A9NNL>
2025-05-24 17:22:37 -0500

    Inicio de sesión

--- Inventario/src/GuiPrincipal/GuiInventario.java
+++ Inventario/src/GuiPrincipal/GuiInventario.java
@@ -14,12 +14,17 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Color;
 
 public class GuiInventario extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JMenuItem menu_Producto;
+	private JLabel lblNewLabel;
 
 	/**
 	 * Launch the application.
@@ -54,10 +59,17 @@ public class GuiInventario extends JFrame implements ActionListener {
 		menu_Producto.addActionListener(this);
 		mnNewMenu.add(menu_Producto);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(128, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		lblNewLabel = new JLabel("BIENVENIDO");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 23));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(128, 11, 183, 43);
+		contentPane.add(lblNewLabel);
 	}
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == menu_Producto) {
