a420d29bd32d12388db2305d228df0627c547470
JPR0GRAMMER <jaircm250606@gmail.com>
2025-06-22 21:41:49 -0500

    more changes.

--- Inventario/src/mysql/ProductoDAO.java
+++ Inventario/src/mysql/ProductoDAO.java
@@ -25,7 +25,8 @@ public class ProductoDAO {
 	public Boolean createProd(Producto p) {
 		Boolean isCreated = true;
 
-		try (Connection con = conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sqlCreate)) {
+		try (Connection con = conexion.getConnection();
+				PreparedStatement ps = con.prepareStatement(sqlCreate);) {
 			ps.setString(1, p.getCodigoProducto());
 			ps.setInt(2, Integer.parseInt(p.getProd()));
 			ps.setInt(3, Integer.parseInt(p.getUnidadMedida()));
@@ -36,8 +37,8 @@ public class ProductoDAO {
 			ps.setDouble(8, p.getCostoBase());
 			ps.setDouble(9, p.getPorcentMargen());
 			ps.setDouble(10, p.getCantidadMedida());
-			ps.setDate(11, new java.sql.Date(p.getFechaFabricacion().getTime()));
-			ps.setDate(12, new java.sql.Date(p.getFechaVencimiento().getTime()));
+			ps.setDate(11, new Date(p.getFechaFabricacion().getTime()));
+			ps.setDate(12, new Date(p.getFechaVencimiento().getTime()));
 			ps.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -52,7 +53,7 @@ public class ProductoDAO {
 
 		try (Connection con = conexion.getConnection();
 				PreparedStatement ps = con.prepareStatement(sqlSelect);
-				ResultSet rs = ps.executeQuery()) {
+				ResultSet rs = ps.executeQuery();) {
 
 			while (rs.next()) {
 				Producto p = new Producto();
@@ -83,30 +84,32 @@ public class ProductoDAO {
 		Producto producto = null;
 		String sqlRead = sqlSelect + "WHERE codigo_producto = ?";
 
-		try (Connection con = conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sqlRead)) {
+		try (Connection con = conexion.getConnection(); 
+				PreparedStatement ps = con.prepareStatement(sqlRead);) {
 
 			ps.setString(1, codigo);
-
-			ResultSet rs = ps.executeQuery();
-
-			if (rs.next()) {
-				producto = new Producto();
-				producto.setIdProducto(rs.getInt("id_producto"));
-				producto.setCodigoProducto(rs.getString("codigo_producto"));
-				producto.setProd(rs.getString("nombre"));
-				producto.setUnidadMedida(rs.getString("unidad_medida"));
-				producto.setPresentacion(rs.getString("presentacion"));
-				producto.setMarca(rs.getString("marca"));
-				producto.setStock(rs.getInt("stock"));
-				producto.setStockMin(rs.getInt("stock_min"));
-				producto.setCostoBase(rs.getDouble("costo_base"));
-				producto.setPorcentMargen(rs.getDouble("porcent_margen"));
-				producto.setCantidadMedida(rs.getDouble("cantidad_medida"));
-				producto.setFechaFabricacion(rs.getDate("fecha_fabricacion"));
-				producto.setFechaVencimiento(rs.getDate("fecha_vencimiento"));
-				producto.setCreatedAt(String.valueOf(rs.getTimestamp("created_at")));
-				producto.setUpdatedAt(String.valueOf(rs.getTimestamp("updated_at")));
+			
+			try(ResultSet rs = ps.executeQuery();){
+				if (rs.next()) {
+					producto = new Producto();
+					producto.setIdProducto(rs.getInt("id_producto"));
+					producto.setCodigoProducto(rs.getString("codigo_producto"));
+					producto.setProd(rs.getString("nombre"));
+					producto.setUnidadMedida(rs.getString("unidad_medida"));
+					producto.setPresentacion(rs.getString("presentacion"));
+					producto.setMarca(rs.getString("marca"));
+					producto.setStock(rs.getInt("stock"));
+					producto.setStockMin(rs.getInt("stock_min"));
+					producto.setCostoBase(rs.getDouble("costo_base"));
+					producto.setPorcentMargen(rs.getDouble("porcent_margen"));
+					producto.setCantidadMedida(rs.getDouble("cantidad_medida"));
+					producto.setFechaFabricacion(rs.getDate("fecha_fabricacion"));
+					producto.setFechaVencimiento(rs.getDate("fecha_vencimiento"));
+					producto.setCreatedAt(String.valueOf(rs.getTimestamp("created_at")));
+					producto.setUpdatedAt(String.valueOf(rs.getTimestamp("updated_at")));
+					}
 			}
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -117,30 +120,32 @@ public class ProductoDAO {
 		List<Producto> productos = new ArrayList<>();
 
 		String sqlFilt = sqlSelect + (prod.equals("-1")? "" : "WHERE p.id_prod_gen = ?");
-		try (Connection con = conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sqlFilt)) {
+		try (Connection con = conexion.getConnection(); 
+				PreparedStatement ps = con.prepareStatement(sqlFilt);
+				) {
 			
-			if(!prod.equals("-1")) ps.setString(1, prod);
+			if(!prod.equals("-1")) ps.setInt(1, Integer.parseInt(prod));
 			
-			ResultSet rs = ps.executeQuery();
-
-			while (rs.next()) {
-				Producto p = new Producto();
-				p.setIdProducto(rs.getInt("id_producto"));
-				p.setCodigoProducto(rs.getString("codigo_producto"));
-				p.setProd(rs.getString("nombre"));
-				p.setUnidadMedida(rs.getString("unidad_medida"));
-				p.setPresentacion(rs.getString("presentacion"));
-				p.setMarca(rs.getString("marca"));
-				p.setStock(rs.getInt("stock"));
-				p.setStockMin(rs.getInt("stock_min"));
-				p.setCostoBase(rs.getDouble("costo_base"));
-				p.setPorcentMargen(rs.getDouble("porcent_margen"));
-				p.setCantidadMedida(rs.getDouble("cantidad_medida"));
-				p.setFechaFabricacion(rs.getDate("fecha_fabricacion"));
-				p.setFechaVencimiento(rs.getDate("fecha_vencimiento"));
-				p.setCreatedAt(String.valueOf(rs.getTimestamp("created_at")));
-				p.setUpdatedAt(String.valueOf(rs.getTimestamp("updated_at")));
-				productos.add(p);
+			try(ResultSet rs = ps.executeQuery();){
+				while (rs.next()) {
+					Producto p = new Producto();
+					p.setIdProducto(rs.getInt("id_producto"));
+					p.setCodigoProducto(rs.getString("codigo_producto"));
+					p.setProd(rs.getString("nombre"));
+					p.setUnidadMedida(rs.getString("unidad_medida"));
+					p.setPresentacion(rs.getString("presentacion"));
+					p.setMarca(rs.getString("marca"));
+					p.setStock(rs.getInt("stock"));
+					p.setStockMin(rs.getInt("stock_min"));
+					p.setCostoBase(rs.getDouble("costo_base"));
+					p.setPorcentMargen(rs.getDouble("porcent_margen"));
+					p.setCantidadMedida(rs.getDouble("cantidad_medida"));
+					p.setFechaFabricacion(rs.getDate("fecha_fabricacion"));
+					p.setFechaVencimiento(rs.getDate("fecha_vencimiento"));
+					p.setCreatedAt(String.valueOf(rs.getTimestamp("created_at")));
+					p.setUpdatedAt(String.valueOf(rs.getTimestamp("updated_at")));
+					productos.add(p);
+				}
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -148,37 +153,61 @@ public class ProductoDAO {
 		return productos;
 	}
 
-	public void actualizarProducto(Producto p) {
-		/*
-		 * String sql =
-		 * "UPDATE productos SET id_prod_gen=?, id_uni_medi=?, id_present=?, id_marca=?, stock=?, stock_min=?, "
-		 * +
-		 * "costo_base=?, porcent_margen=?, cantidad_medida=?, fecha_fabricacion=?, fecha_vencimiento=?"
-		 * + "WHERE codigo_producto=?";
-		 * 
-		 * try (Connection con = conexion.getConnection(); PreparedStatement ps =
-		 * con.prepareStatement(sql)) { ps.setInt(1, p.getIdProdGen()); ps.setInt(2,
-		 * p.getIdUniMedi()); ps.setInt(3, p.getIdPresent()); ps.setInt(4,
-		 * p.getIdMarca()); ps.setInt(5, p.getStock()); ps.setInt(6, p.getStockMin());
-		 * ps.setDouble(7, p.getCostoBase()); ps.setDouble(8, p.getPorcentMargen());
-		 * ps.setDouble(9, p.getCantidadMedida()); ps.setDate(10, new
-		 * java.sql.Date(p.getFechaFabricacion().getTime())); ps.setDate(11, new
-		 * java.sql.Date(p.getFechaVencimiento().getTime())); ps.setString(12,
-		 * p.getCodigoProducto()); ps.executeUpdate();
-		 * System.out.println("✅ Producto actualizado"); } catch (SQLException e) {
-		 * e.printStackTrace(); }
-		 */
+	public Boolean updateProd(Producto p) {
+	    Boolean isUpdated = true;
+	    String sqlUpdate = "UPDATE productos SET " +
+	            "codigo_producto = ?, " +
+	            "id_prod_gen = ?, " +
+	            "id_uni_medi = ?, " +
+	            "id_present = ?, " +
+	            "id_marca = ?, " +
+	            "stock = ?, " +
+	            "stock_min = ?, " +
+	            "costo_base = ?, " +
+	            "porcent_margen = ?, " +
+	            "cantidad_medida = ?, " +
+	            "fecha_fabricacion = ?, " +
+	            "fecha_vencimiento = ? " +
+	            "WHERE id_producto = ?";
+
+	    try (Connection con = conexion.getConnection();
+	    		PreparedStatement ps = con.prepareStatement(sqlUpdate);) {
+	        ps.setString(1, p.getCodigoProducto());
+	        ps.setInt(2, Integer.parseInt(p.getProd()));
+	        ps.setInt(3, Integer.parseInt(p.getUnidadMedida()));
+	        ps.setInt(4, Integer.parseInt(p.getPresentacion()));
+	        ps.setInt(5, Integer.parseInt(p.getMarca()));
+	        ps.setInt(6, p.getStock());
+	        ps.setInt(7, p.getStockMin());
+	        ps.setDouble(8, p.getCostoBase());
+	        ps.setDouble(9, p.getPorcentMargen());
+	        ps.setDouble(10, p.getCantidadMedida());
+	        ps.setDate(11, new Date(p.getFechaFabricacion().getTime()));
+	        ps.setDate(12, new Date(p.getFechaVencimiento().getTime()));
+	        ps.setInt(13, p.getIdProducto());
+	        ps.executeUpdate();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	        isUpdated = false;
+	    }
+
+	    return isUpdated;
 	}
 
-	public void eliminarProducto(String codigo) {
-		String sql = "DELETE FROM productos WHERE codigo_producto=?";
+	public Boolean deleteProd(String prod) {
+		Boolean isDeleted = true;
+		
+		String sqlDelete = "DELETE FROM productos WHERE id_producto = ?";
 
-		try (Connection con = conexion.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
-			ps.setString(1, codigo);
+		try (Connection con = conexion.getConnection();
+				PreparedStatement ps = con.prepareStatement(sqlDelete);) {
+			ps.setInt(1, Integer.parseInt(prod));
 			ps.executeUpdate();
-			System.out.println("🗑️ Producto eliminado");
 		} catch (SQLException e) {
 			e.printStackTrace();
+			isDeleted = false;
 		}
+		
+		return isDeleted;
 	}
 }
